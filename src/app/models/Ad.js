2d17071c937853084445a0d743e0939af7d6d404
Rafael de Oliveira <rafaoliverdev@gmail.com>
2019-02-24 15:00:30 -0300

    Ad > Model: created.

---
+++ src/app/models/Ad.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const Ad = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  author: {
+    // for more than one Author, use Array instead of Object
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model('Ad', Ad)
