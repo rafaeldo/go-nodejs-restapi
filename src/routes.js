15787518a2a2271e5899b594597a104d0376c1e4
Rafael de Oliveira <rafaoliverdev@gmail.com>
2019-02-24 14:09:27 -0300

    Session > Controller: store moethod created. Route sessions created.

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,10 @@ const routes = express.Router()
 
 // Controllers
 const UserController = require('./app/controllers/UserController')
+const SessionController = require('./app/controllers/SessionController')
 
 // ROUTES
 routes.post('/users', UserController.store)
+routes.post('/sessions', SessionController.store)
 
 module.exports = routes
