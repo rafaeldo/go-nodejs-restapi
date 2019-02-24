6377b4282223a0dfa2a0bbc9e1346370ba54b595
Rafael de Oliveira <rafaoliverdev@gmail.com>
2019-02-24 13:31:44 -0300

    User > Controller: created. Route > User > Create working.

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,10 @@
 const express = require('express')
-
 const routes = express.Router()
+
+// Controllers
+const UserController = require('./app/controllers/UserController')
+
+// ROUTES
+routes.post('/users', UserController.store)
 
 module.exports = routes
